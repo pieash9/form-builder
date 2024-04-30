cb4bd1e315d34979c2883092453b71d21e0e1566
Md. pieash ahmed <81944951+pieash9@users.noreply.github.com>
2024-04-30 23:56:43 +0600

    setup prisma postgres with vercel

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,5 +1,14 @@
+import Link from "next/link";
+
 const Logo = () => {
-  return <div></div>;
+  return (
+    <Link
+      href="/"
+      className="font-bold text-3xl bg-gradient-to-r from-indigo-400 to-cyan-400 text-transparent bg-clip-text"
+    >
+      Form Builder
+    </Link>
+  );
 };
 
 export default Logo;
