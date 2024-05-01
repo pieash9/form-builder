c147868e01331222938a5f38c8a9a4974ac7f0d6
Md. pieash ahmed <81944951+pieash9@users.noreply.github.com>
2024-05-01 23:02:15 +0600

    added designer component UI and context

--- components/Designer.tsx
+++ components/Designer.tsx
@@ -2,13 +2,37 @@
 
 import { cn } from "@/lib/utils";
 import DesignerSidebar from "./DesignerSidebar";
-import { useDroppable } from "@dnd-kit/core";
+import { DragEndEvent, useDndMonitor, useDroppable } from "@dnd-kit/core";
+import useDesigner from "./hooks/useDesigner";
+import {
+  ElementsType,
+  FormElementInstance,
+  FormElements,
+} from "./FormElements";
+import { idGenerator } from "@/lib/idGenerator";
 
 const Designer = () => {
+  const { addElement, elements } = useDesigner();
   const droppable = useDroppable({
     id: "designer-drop-area",
     data: {
       isDesignerDropArea: true,
+    },
+  });
+  console.log({ elements });
+  useDndMonitor({
+    onDragEnd: (event: DragEndEvent) => {
+      const { active, over } = event;
+      if (!active || !over) return;
+
+      const designerBtnElement = active.data?.current?.isDesignerBtnElement;
+      if (designerBtnElement) {
+        const type = active.data?.current?.type;
+        const newElement = FormElements[type as ElementsType].construct(
+          idGenerator()
+        );
+        addElement(0, newElement);
+      }
     },
   });
   return (
@@ -21,14 +45,23 @@ const Designer = () => {
             droppable.isOver && "ring-2 ring-primary/20"
           )}
         >
-          {droppable.isOver ? (
-            <div className="p-4 w-full">
-              <div className="h-[120px] rounded-md bg-primary/20"></div>
-            </div>
-          ) : (
+          {!droppable.isOver && elements.length === 0 && (
             <p className="text-3xl text-muted-foreground flew flex-grow items-center font-bold">
               Drop here
             </p>
+          )}
+          {droppable.isOver && (
+            <div className="p-4 w-full">
+              <div className="h-[120px] rounded-md bg-primary/20"></div>
+            </div>
+          )}
+
+          {elements.length > 0 && (
+            <div className="w-full flex flex-col text-background gap-2 p-4">
+              {elements.map((element) => (
+                <DesignerElementWrapper key={element.id} element={element} />
+              ))}
+            </div>
           )}
         </div>
       </div>
@@ -36,5 +69,10 @@ const Designer = () => {
     </div>
   );
 };
+
+function DesignerElementWrapper({ element }: { element: FormElementInstance }) {
+  const DesignerElement = FormElements[element.type].designerComponent;
+  return <DesignerElement />;
+}
 
 export default Designer;
--- components/fields/TextField.tsx
+++ components/fields/TextField.tsx
@@ -21,7 +21,7 @@ export const TextFieldFormElement: FormElement = {
     icon: MdTextFields,
     label: "Text Field",
   },
-  designerComponent: () => <div>Designer Component</div>,
+  designerComponent: () => <div className="text-white">Designer Component</div>,
   formComponent: () => <div>Form Component</div>,
   propertiesComponent: () => <div>Properties Component</div>,
 };
