de95815811dab36ae80944278ce09446fd34c033
Md. pieash ahmed <81944951+pieash9@users.noreply.github.com>
2024-05-01 09:41:26 +0600

    added home page stats of form

---
+++ actions/form.ts
@@ -0,0 +1,45 @@
+"use server";
+
+import prisma from "@/lib/prisma";
+import { currentUser } from "@clerk/nextjs/server";
+
+class UserNotFoundErr extends Error {}
+
+export async function GetFormStats() {
+  const user = await currentUser();
+
+  if (!user) {
+    throw new UserNotFoundErr();
+  }
+
+  const stats = prisma.form.aggregate({
+    where: {
+      userId: user.id,
+    },
+    _sum: {
+      visits: true,
+      submissions: true,
+    },
+  });
+
+  const visits = (await stats)._sum.visits || 0;
+  const submissions = (await stats)._sum.submissions || 0;
+
+  let submissionRate = 0;
+  let bounceRate = 0;
+
+  if (visits > 0) {
+    submissionRate = (submissions / visits) * 100;
+  }
+
+  if (submissionRate > 0) {
+    bounceRate = 100 - submissionRate;
+  }
+
+  return {
+    visits,
+    submissions,
+    submissionRate,
+    bounceRate,
+  };
+}
--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,5 +1,117 @@
-import Image from "next/image";
+import { GetFormStats } from "@/actions/form";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Skeleton } from "@/components/ui/skeleton";
+import { ReactNode, Suspense } from "react";
+import { LuView } from "react-icons/lu";
+import { FaWpforms } from "react-icons/fa";
+import { TbArrowBounce } from "react-icons/tb";
+import { HiCursorClick } from "react-icons/hi";
+import { Separator } from "@/components/ui/separator";
 
-export default function Home() {
-  return <main className="">Home</main>;
+export default function HomePage() {
+  return (
+    <div className="container p-4">
+      <Suspense fallback={<StatsCards loading={true} />}>
+        <CardStatsWrapper />
+      </Suspense>
+      <Separator className="my-6" />
+      <h2 className="text-4xl font-bold col-span-2">Your Forms</h2>
+      <Separator className="my-6" />
+    </div>
+  );
+}
+
+async function CardStatsWrapper() {
+  const status = await GetFormStats();
+
+  return <StatsCards data={status} loading={false} />;
+}
+
+interface StatsCardProps {
+  data?: Awaited<ReturnType<typeof GetFormStats>>;
+  loading: boolean;
+}
+
+function StatsCards(props: StatsCardProps) {
+  const { data, loading } = props;
+
+  return (
+    <div className="w-full pt-8 grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
+      <StatsCard
+        title="Total visits"
+        icons={<LuView className="text-blue-600" />}
+        helperText="All time form visits"
+        value={data?.visits.toLocaleString() || ""}
+        loading={loading}
+        className="shadow-md shadow-blue-600"
+      />
+
+      <StatsCard
+        title="Total submissions"
+        icons={<FaWpforms className="text-yellow-600" />}
+        helperText="All time form submissions"
+        value={data?.submissions.toLocaleString() || ""}
+        loading={loading}
+        className="shadow-md shadow-yellow-600"
+      />
+
+      <StatsCard
+        title="Submission rate"
+        icons={<HiCursorClick className="text-green-600" />}
+        helperText="Visit that result in form submission"
+        value={data?.submissionRate.toLocaleString() + "%" || ""}
+        loading={loading}
+        className="shadow-md shadow-green-600"
+      />
+
+      <StatsCard
+        title="Bounce rate"
+        icons={<TbArrowBounce className="text-red-600" />}
+        helperText="Visit that leaves without interacting"
+        value={data?.bounceRate.toLocaleString() + "%" || ""}
+        loading={loading}
+        className="shadow-md shadow-red-600"
+      />
+    </div>
+  );
+}
+
+function StatsCard({
+  title,
+  icons,
+  helperText,
+  value,
+  loading,
+  className,
+}: {
+  title: string;
+  icons: ReactNode;
+  helperText: string;
+  value: string;
+  loading: boolean;
+  className?: string;
+}) {
+  return (
+    <Card className={className}>
+      <CardHeader className="flex flex-row items-center justify-between pb-2">
+        <CardTitle className="text-sm font-medium text-muted-foreground">
+          {title}
+        </CardTitle>
+        {icons}
+      </CardHeader>
+
+      <CardContent>
+        <div className="text-2xl font-bold">
+          {loading ? (
+            <Skeleton>
+              <span className="opacity-0">0</span>
+            </Skeleton>
+          ) : (
+            <span>{value}</span>
+          )}
+        </div>
+        <p className="text-xs text-muted-foreground pt-1">{helperText}</p>
+      </CardContent>
+    </Card>
+  );
 }
