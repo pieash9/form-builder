597f4dc152033ad9acb8dbffc6eb2f6f860dc66f
Md. pieash ahmed <81944951+pieash9@users.noreply.github.com>
2024-05-07 09:49:32 +0600

    added saved and publish functionality

--- components/FormBuilder.tsx
+++ components/FormBuilder.tsx
@@ -16,6 +16,12 @@ import DragOverlayWrapper from "./DragOverlayWrapper";
 import { useEffect, useState } from "react";
 import useDesigner from "./hooks/useDesigner";
 import { ImSpinner2 } from "react-icons/im";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import { toast } from "./ui/use-toast";
+import Link from "next/link";
+import { BsArrowLeft, BsArrowRight } from "react-icons/bs";
+import Confetti from "react-confetti";
 
 const FormBuilder = ({ form }: { form: Form }) => {
   const [isReady, setIsReady] = useState(false);
@@ -52,6 +58,60 @@ const FormBuilder = ({ form }: { form: Form }) => {
       <div className="size-full flex items-center justify-center">
         <ImSpinner2 className="animate-spin size-12" />
       </div>
+    );
+  }
+
+  const shareUrl = `${window.location.origin}/submit/${form.shareUrl}`;
+
+  if (form.published) {
+    return (
+      <>
+        <Confetti
+          width={window.innerWidth}
+          height={window.innerHeight}
+          recycle={false}
+          numberOfPieces={500}
+        />
+        <div className="size-full flex items-center justify-center">
+          <div className="max-w-lg">
+            <h1 className="text-center text-4xl font-bold text-primary border-b pb-2 mb-10">
+              🎊🎊 Form Published 🎊🎊
+            </h1>
+            <h2 className="text-2xl">Share this form</h2>
+            <h3 className="text-xl text-muted-foreground border-b pb-10">
+              Anyone with the link can view and submit the form.
+            </h3>
+            <div className="my-4 flex flex-col gap-2 items-center w-full border-b pb-4">
+              <Input className="w-full" readOnly value={shareUrl} />
+              <Button
+                className="mt-2 w-full"
+                onClick={() => {
+                  navigator.clipboard.writeText(shareUrl);
+                  toast({
+                    title: "Copied",
+                    description: "Link copied to clipboard",
+                  });
+                }}
+              >
+                Copy Link
+              </Button>
+            </div>
+            <div className="flex justify-between">
+              <Button variant="link" asChild>
+                <Link href="/" className="gap-2">
+                  <BsArrowLeft /> Go back home
+                </Link>
+              </Button>
+              <Button variant="link" asChild>
+                <Link href={`/forms/${form.id}`} className="gap-2">
+                  Form details
+                  <BsArrowRight />
+                </Link>
+              </Button>
+            </div>
+          </div>
+        </div>
+      </>
     );
   }
 
